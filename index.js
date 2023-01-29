ed445acaffa2e689a6e4fe63cf41e1fa29f51d30
adilabbas135 <adilabbas8092@gmail.com>
2023-01-29 15:36:03 +0500

    cors error

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const app = express();
 const connect_to_db = require("./db");
 var cors = require("cors");
 var cookieParser = require("cookie-parser");
-
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 // Middlewears
 connect_to_db();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
-// app.listen(8800, () => {
-//   console.log("the app is running at port " + 8800);
-// });
-app.use("/", async (req, res) => {
-  return res.status(200).json({ success: true, homepage: true });
+app.listen(8800, () => {
+  console.log("the app is running at port " + 8800);
 });
+// app.use("/", async (req, res) => {
+//   return res.status(200).json({ success: true, homepage: true });
+// });
 
 //Create email account and login
 app.use("/account", require("./Routes/auth/account"));
