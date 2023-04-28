4b802e78479df64eda510ea015838f1bc1a7901e
AdilAbbas135 <adilabbas8092@gmail.com>
2023-04-28 22:17:29 +0500

    changes today

--- Routes/find.js
+++ Routes/find.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const TutionModel = require("../Models/Teacher/Tution");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 
+//GET ALL TUTIONS FOR ALL TUTIONS PAGE
 router.get("/tutions", async (req, res) => {
   try {
     const tutions = await TutionModel.aggregate([
@@ -16,6 +18,28 @@ router.get("/tutions", async (req, res) => {
       { $sort: { createdAt: -1 } },
     ]);
     return res.status(200).json(tutions);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// GET SINGLE TUTION DETAIL FOR SINGLE TUTION PAGE
+router.get("/tutions/singleTution", async (req, res) => {
+  try {
+    const TutionId = req.query.TutionId;
+    const tution = await TutionModel.aggregate([
+      { $match: { _id: mongoose.Types.ObjectId(TutionId) } },
+      {
+        $lookup: {
+          from: "teachers",
+          localField: "TeacherId",
+          foreignField: "_id",
+          as: "Teacher",
+        },
+      },
+      { $sort: { createdAt: -1 } },
+    ]);
+    return res.status(200).json(tution[0]);
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });
   }
